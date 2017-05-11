7e8e5827310e672bb76649e6a5ded01f21a2d96d
Unknown <jesusapodacaatondo@hotmail.com>
2017-05-11 10:11:08 -0600

    Proyecto base

---
+++ e-Shop/e-Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_Shop.Startup))]
+namespace e_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
