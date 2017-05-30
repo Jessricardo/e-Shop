1551cf85eee96e017ae771cd8174a152b2413696
Unknown <jesusapodacaatondo@hotmail.com>
2017-05-29 22:57:39 -0600

    Categorias ready

---
+++ e-Shop/e-Shop/Controllers/CategoriesController.cs
@@ -0,0 +1,88 @@
+﻿using e_Shop.Models;
+using e_Shop.Repository;
+using Microsoft.Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Shop.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private CategoriasRepository categorias;
+        private IProductosRepository productos;
+        public CategoriesController()
+        {
+            productos = new AzureProductosRepository(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            categorias = new CategoriasRepository();
+        }
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(categorias.Todaslascategorias());
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Categories/Details/categoria
+        public ActionResult Details(string id)
+        {
+            List<ProductoModel> listaProductos = productos.LeerProductos();
+            List<ProductoModel> nuevaLista = new List<ProductoModel>();
+            foreach(ProductoModel prod in listaProductos)
+            {
+                if (prod.Categoria == id)
+                {
+                    nuevaLista.Add(prod);
+                }
+            }
+            return View(nuevaLista);
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        public ActionResult Create(string categoria)
+        {
+            try
+            {
+                categorias.AgregarCategoria(categoria);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(string categoria)
+        {
+            categorias.EliminarCategoria(categoria);
+            return RedirectToAction("Index");
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string categoria, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                categorias.EliminarCategoria(categoria);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ e-Shop/e-Shop/Repository/CategoriasRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace e_Shop.Repository
+{
+    public class CategoriasRepository
+    {
+        private static List<string> categorias;
+        static CategoriasRepository()
+        {
+            categorias = new List<string>();
+            categorias.Add("Xbox");
+            categorias.Add("Xbox One");
+            categorias.Add("Wii U");
+            categorias.Add("PlayStation 4");
+        }
+        public void AgregarCategoria(string categoria)
+        {
+            if (!categorias.Contains(categoria))
+            {
+                categorias.Add(categoria);
+            }
+        }
+        public void EliminarCategoria(string categoria)
+        {
+            categorias.Remove(categoria);
+        }
+        public List<string> Todaslascategorias()
+        {
+            return categorias;
+        }
+    }
+}
